2f237e6997db96ddfe6e4c3981b27d28704de6cf
bilalyarmaci <92362207+bilalyarmaci@users.noreply.github.com>
2024-04-14 15:19:23 +0300

    Refactor database services and update routes in app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const { Pool } = require('pg');
-const dotenv = require('dotenv');   // Import dotenv module to read .env file
-const dbQueries = require('./dbServices');
-
+const dotenv = require('dotenv');  
 dotenv.config();    // Read .env file
+
+const dbSetup = require('./databasepg');
+const dbServices = require('./dbServices');
 
 const app = express();
 const port = 3000;
@@ -12,42 +12,42 @@ app.use(express.json());
 
 // Get all students
 app.get('/students', async (req, res) => {
-   dbQueries.getAllStudents(res);
+   dbServices.getAllStudents(res);
 });
 
 // Add a new student
 app.post('/students', async (req, res) => {
-    dbQueries.addStudent(req, res);
+    dbServices.addStudent(req, res);
 });
 
 // Update a student
 app.put('/students/:id', async (req, res) => {
-    dbQueries.updateStudentByID(req, res);
+    dbServices.updateStudentByID(req, res);
 });
 
 // Delete a student
 app.delete('/students/:id', async (req, res) => {
-    dbQueries.deleteStudentByID(req, res);
+    dbServices.deleteStudentByID(req, res);
 });
 
 // Get all departments
 app.get('/departments', async (req, res) => {
-    dbQueries.getAllDepartments(res);
+    dbServices.getAllDepartments(res);
 });
 
 // Add a new department
 app.post('/departments', async (req, res) => {
-    dbQueries.addDepartment(req, res);
+    dbServices.addDepartment(req, res);
 });
 
 // Update a department
 app.put('/departments/:id', async (req, res) => {
-    dbQueries.updateDepartmentByID(req, res);
+    dbServices.updateDepartmentByID(req, res);
 });
 
 // Delete a department
 app.delete('/departments/:id', async (req, res) => {
-    dbQueries.deleteDepartmentByID(req, res);
+    dbServices.deleteDepartmentByID(req, res);
 });
 
 app.listen(port, () => {
